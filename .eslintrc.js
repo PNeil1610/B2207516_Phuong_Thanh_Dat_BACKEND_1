6ae8d1209d09581ad601cb345c2567f4aada46d6
Dat Phuong <bomvp49@gmail.com>
2025-02-26 08:04:23 +0700

    Cau hinh ESlint cho du an

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,4 +6,3 @@ module.exports = {
     },
     extends: ['eslint:recommended', 'prettier'],
 };
-// In the above configuration, we have defined the environment as node, commonjs, and es2021. We have also extended the recommended rules from ESLint and Prettier. This configuration will help us to identify the errors and warnings in our code.
